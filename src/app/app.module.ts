ba4b4d9787aa2aa9da79399aa3423243228c1d9e
erickmt <33611654+erickmt@users.noreply.github.com>
2018-07-16 18:47:33 -0300

    ajuste layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,28 +7,20 @@ import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { AppRoutingModule, routingComponents } from "./app-routing.module";
 import { AppComponent } from './app.component';
 import { MenuLateralComponent } from './menu-lateral/menu-lateral.component';
-<<<<<<< HEAD
-import { FormaPagamentoComponent } from './forma-pagamento/forma-pagamento.component';
-=======
 import { MaterialComponent } from './material/material.component';
 import { FilterFormDirective } from './diretivas/filter-form.directive';
 import { FilterFormComponent } from './filter-form/filter-form.component';
 import { EditFormComponent } from './edit-form/edit-form.component';
->>>>>>> 3453ee80d01d86a67076db28f9678ba01b7f1d75
 
 @NgModule({
   declarations: [
     AppComponent,
     routingComponents,
     MenuLateralComponent,
-<<<<<<< HEAD
-    FormaPagamentoComponent
-=======
     MaterialComponent,
     FilterFormDirective,
     FilterFormComponent,
     EditFormComponent
->>>>>>> 3453ee80d01d86a67076db28f9678ba01b7f1d75
   ],
   imports: [
     BrowserModule,
